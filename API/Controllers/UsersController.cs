3cbc46322e72f43a392ffc9fc981ff0cb11d5f7a
yousra-cardaire <yousra.cardaire@gmail.com>
2024-11-21 16:08:39 +0100

    add authentication, jwt, DTOs, middleware and extensions methods

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API;
+namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
     {
@@ -17,7 +17,8 @@ public class UsersController(DataContext context) : ControllerBase
         return users;
     }
 
-    [HttpGet("{id}")]
+    [Authorize]
+    [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id) 
     {
         var user = await context.Users.FindAsync(id);
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,7 @@
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
